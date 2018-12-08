2fef2102b2fb2ce8ed468dd88ef42e9d145f1cce
Kikikan <kikikkan@freemail.hu>
2018-12-08 17:54:35 +0100

    2 fragment

--- app/src/main/java/net/kikikan/fragmentviewer/MainActivity.java
+++ app/src/main/java/net/kikikan/fragmentviewer/MainActivity.java
@@ -1,7 +1,8 @@
 package net.kikikan.fragmentviewer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ChangeFragment(View view) {
+        if (view == findViewById(R.id.button1)) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment, new FragmentOne()).commit();
+        }
+        else
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment, new FragmentTwo()).commit();
     }
 }
